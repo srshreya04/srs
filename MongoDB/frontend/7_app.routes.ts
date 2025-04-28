57d02efab4b79931a941385663fe29649e54c636
srshreya04 <131507420+srshreya04@users.noreply.github.com>
2025-04-28 16:16:29 +0530

    Create 7_app.routes.ts

---
+++ MongoDB/frontend/7_app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { StudentsComponent } from './students/students.component';
+
+export const routes: Routes = [
+  { path: '', component: StudentsComponent }, // root path shows students
+];
