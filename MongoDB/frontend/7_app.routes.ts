df37f5d0b25c771019927207656d2f669aeb14aa
srshreya04 <131507420+srshreya04@users.noreply.github.com>
2025-11-04 00:31:35 +0530

    Delete MongoDB directory

--- MongoDB/frontend/7_app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { StudentsComponent } from './students/students.component';
-
-export const routes: Routes = [
-  { path: '', component: StudentsComponent }, // root path shows students
-];
